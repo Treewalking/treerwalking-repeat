28337bdcfa9ed839ca399368506ce5077ba9cae5
p.bateman <pat.s.bateman@btinternet.com>
2021-11-23 17:24:17 +0000

    TREEW-6 : Add simple HTTP Client

--- jira/jira.go
+++ jira/jira.go
@@ -1,7 +1,22 @@
 package jira
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("hello world")
+type JIRA struct {
+	Client  *http.Client
+	BaseURL string
+}
+
+func (api *JIRA) DoStuff() ([]byte, error) {
+	resp, err := api.Client.Get(api.BaseURL + "/some/path")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	// handling error and doing stuff with body that needs to be unit tested
+	return body, err
 }
--- jira/jira_test.go
+++ jira/jira_test.go
@@ -1,7 +1,27 @@
 package jira_test
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"treerwalking.org/repeat/jira"
+)
 
-func TestAddWithTestingPackage(t *testing.T) {
+func TestDoStuffWithTestServer(t *testing.T) {
+	// Start a local HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Test request parameters
+		assert.Equal(t, req.URL.String(), "/some/path")
+		// Send response to be tested
+		rw.Write([]byte(`OK`))
+	}))
+	// Close the server when test finishes
+	defer server.Close()
 
+	// Use Client & URL from our local test server
+	api := jira.JIRA{server.Client(), server.URL}
+	body, _ := api.DoStuff()
+
+	assert.Equal(t, []byte("OK"), body)
 }
